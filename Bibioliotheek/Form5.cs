b16197abf2d4d661175ed1ac497aae1849f0369f
Rayan <roo.french17@gmail.com>
2024-06-08 21:15:25 +0200

    login being worked on

--- Bibioliotheek/Form5.cs
+++ Bibioliotheek/Form5.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,9 +20,52 @@ namespace Bibioliotheek
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            string username = Convert.ToString(txtusername.Text);
-            string password = Convert.ToString(txtwachtwoord.Text);
 
+            string connectionString = "server=localhost;database=filmproject;uid=root;pwd='';";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+
+            try
+            {
+                string username = Convert.ToString(txtusername.Text);
+                string password = Convert.ToString(txtwachtwoord.Text);
+
+                connection.Open();
+
+                if (username.Equals(""))
+                {
+                    MessageBox.Show("Give in a username");
+                    txtusername.Focus();
+                }
+                else if (password.Equals(""))
+                {
+                    MessageBox.Show("Give in a password");
+                    txtwachtwoord.Focus();
+                }
+                else
+                {
+                    string qry = "INSERT INTO tblklant (klantnaam, gebruikernaam, wachtwoord) VALUES (?,?,?)";
+                    MySqlCommand command = new MySqlCommand(qry, connection);
+                    command.Parameters.AddWithValue("", username);
+                    command.Parameters.AddWithValue("", password);
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Succesfully logged in");
+                    frmMain mainForm = new frmMain();
+                    mainForm.Show();
+                    this.Hide();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
 
         }
     }
